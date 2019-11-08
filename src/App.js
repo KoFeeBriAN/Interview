2aac648ff5b3376502939fcdc96a3d06d9773bf7
KoFeeBriAN <coffeetk2017@gmail.com>
2019-11-08 18:17:57 +0700

    work! version 1.0.0

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useState } from 'react';
+import React, { useReducer } from 'react';
 
 import './App.css';
 import { Input, Select, Button } from 'antd';
@@ -10,26 +10,62 @@ const initFormData = {
   name: '',
   email: '',
   phone: '',
-  male: ''
+  male: 'male'
 };
 
-const handleSubmit = formData => {
-  // submit formData using submitAPI
-  const { fullname, email, phone } = formData;
+class Validatior {
+  constructor() {}
+  re = {
+    email: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    phone: /^0[0-9]{2}-[0-9]{3}-[0-9]{4}/
+  };
 
-  console.log(formData);
-};
+  validateName = name => name.length > 0 && name.length <= 255;
+  validateEmail = email => {
+    console.assert(this.re);
+    return this.re.email.test(email);
+  };
+  validatePhone = phone => this.re.phone.test(phone);
+  validateMale = male => ['male', 'female'].includes(male) !== -1;
+}
 
 const formReducer = (formData = initFormData, action) => {
-  formData = { ...formData };
+  return { ...formData, ...action.payload };
 };
 
 export default () => {
   const [formData, dispatch] = useReducer(formReducer, initFormData);
 
+  const handleSubmit = async formData => {
+    // submit formData using submitAPI
+    const v = new Validatior();
+    const { name, email, phone, male } = formData;
+    let errors = [];
+
+    if (!v.validateName(name.trim())) errors.push('Invalid Name');
+    if (!v.validateEmail(email.trim().toLowerCase()))
+      errors.push('Invalid Email');
+    if (!v.validatePhone(phone.trim())) errors.push('Invalid Phone');
+    if (!v.validateMale(male.trim())) errors.push('Invalid Male');
+
+    if (errors.length !== 0)
+      return dispatch({
+        payload: { ...formData, errorMessage: errors.join(', ') }
+      });
+    else {
+      submitAPI({ name, email, phone, male });
+      dispatch({ payload: initFormData }); // reset
+    }
+  };
+
   const handleChange = e => {
-    const { name, value } = e.target;
-    setState({ ...state, [name]: value });
+    dispatch({
+      payload: { ...formData, [e.target.name]: e.target.value }
+    });
+  };
+
+  const handleSelectMaleChange = male => {
+    dispatch({ payload: { ...formData, male } });
   };
 
   return (
@@ -38,26 +74,30 @@ export default () => {
         {formData.errorMessage}
       </h1>
       <Input
-        name='fullname'
-        onChange={e => dispatch({ formData, action: e.target.name })}
+        name='name'
+        value={formData.name}
+        onChange={handleChange}
         className='input'
         placeholder='ชื่อ - นามสกุล'
       />
       <Input
         name='email'
-        onChange={e => dispatch({ formData, action: e.target.name })}
+        value={formData.email}
+        onChange={handleChange}
         className='input'
         placeholder='email'
       />
       <Input
         name='phone'
-        onChange={e => dispatch({ formData, action: e.target.name })}
+        value={formData.phone}
+        onChange={handleChange}
         className='input'
         placeholder='phone'
       />
       <Select
         name='male'
-        onChange={e => dispatch({ formData, action: e.target.name })}
+        value={formData.male}
+        onChange={handleSelectMaleChange}
         className='input'
         defaultValue='male'
       >
@@ -65,7 +105,7 @@ export default () => {
         <Option value='female'>หญิง</Option>
       </Select>{' '}
       <br />
-      <Button onSubmit={() => handleSubmit(formData)} type='primary'>
+      <Button onClick={() => handleSubmit(formData)} type='primary'>
         submit
       </Button>
     </div>
